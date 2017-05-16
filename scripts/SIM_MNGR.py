096dfdab22c53d7a49595318e8c0a2203a7d7bf8
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2017-05-16 10:49:49 +0200

    Added another pizza_demo child

--- scripts/SIM_MNGR.py
+++ scripts/SIM_MNGR.py
@@ -43,8 +43,8 @@ rpc_server = RPCServerGreenlets(
 #This should be read out of some config info
 #childNums = (0, 1)
 #childPackages = ('pizza_demo', 'prac_cs_demo')
-childNums = (1, 2)
-childPackages = ('pizza_demo', 'prac_cs_demo')
+childNums = (1, 2, 3)
+childPackages = ('pizza_demo', 'prac_cs_demo', 'pizza_demo')
 packageActions = {'pizza_demo': ['Cutting'], 'prac_cs_demo': ['Pouring', 'Pipetting', 'UsingMeasuringCup']}
 packageURDFs = {'pizza_demo': [['robot_description_nocol', '', 'http://svn.ai.uni-bremen.de/svn/sim_models/'],
                                ['kitchen_description', 'iai_kitchen', 'http://svn.ai.uni-bremen.de/svn/sim_models/'],
